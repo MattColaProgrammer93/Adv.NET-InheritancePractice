d20a9e6336483378706b96a67d32d384d3585466
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-17 13:05:53 -0700

    Created the button that displays candy and create a listbox to display the list of candies

--- InheritancePractice/Form1.Designer.cs
+++ InheritancePractice/Form1.Designer.cs
@@ -28,20 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnDisplayCandy = new System.Windows.Forms.Button();
+            this.lstCandies = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // btnDisplayCandy
+            // 
+            this.btnDisplayCandy.Location = new System.Drawing.Point(74, 34);
+            this.btnDisplayCandy.Name = "btnDisplayCandy";
+            this.btnDisplayCandy.Size = new System.Drawing.Size(127, 69);
+            this.btnDisplayCandy.TabIndex = 0;
+            this.btnDisplayCandy.Text = "Display Candy";
+            this.btnDisplayCandy.UseVisualStyleBackColor = true;
+            // 
+            // lstCandies
+            // 
+            this.lstCandies.FormattingEnabled = true;
+            this.lstCandies.ItemHeight = 20;
+            this.lstCandies.Location = new System.Drawing.Point(259, 34);
+            this.lstCandies.Name = "lstCandies";
+            this.lstCandies.Size = new System.Drawing.Size(214, 144);
+            this.lstCandies.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstCandies);
+            this.Controls.Add(this.btnDisplayCandy);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Button btnDisplayCandy;
+        private ListBox lstCandies;
     }
 }
