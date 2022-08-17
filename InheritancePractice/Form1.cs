4b435b5cd8321c1d77a04348815f17599f09dfc5
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-17 12:57:35 -0700

    Add project files.

---
+++ InheritancePractice/Form1.cs
@@ -0,0 +1,15 @@
+namespace InheritancePractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+        }
+    }
+}
